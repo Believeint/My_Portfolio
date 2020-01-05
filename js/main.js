2b08e7b76f639321f3acc4411de3163790b6bf18
elias.santos <elias.carvalho.096@gmail.com>
2020-01-04 22:54:23 -0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+
+const navItems = document.querySelectorAll(".nav-item");
+
+// Setar estado do menu para falso
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(items => items.classList.add("show"));
+    // Setar estado do menu
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(items => items.classList.remove("show"));
+    // Setar estado do menu
+    showMenu = false;
+  }
+}
